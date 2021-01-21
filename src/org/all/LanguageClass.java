fb8e90c9ca0931491481deb78c400a26bab7961c
Mohanram77 <mohanram.m1998@gmail.com>
2021-01-21 17:39:44 +0530

    hi mohan

---
+++ src/org/all/LanguageClass.java
@@ -0,0 +1,10 @@
+package org.all;
+
+public class LanguageClass {
+public void allLanguage() {
+System.out.println("Tamil");
+System.out.println("english");
+System.out.println("hindi");
+}
+	
+}
---
+++ src/org/telugu/Telugu.java
@@ -0,0 +1,16 @@
+package org.telugu;
+
+import org.english.English;
+
+public class Telugu extends English {
+	private void teluguLanguage() {
+System.out.println("lang:telugu");
+	}
+public static void main(String[] args) {
+	Telugu a=new Telugu();
+	a.teluguLanguage();
+	a.englishLanguage();
+	a.tamilLanguage();
+	a.allLanguage();
+}
+}
